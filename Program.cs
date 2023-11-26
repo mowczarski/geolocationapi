6976861de5d48826144e403cd26f979eb6ae669a
mowczarski <mowczarski591@gmail.com>
2023-11-26 10:51:44 +0100

    init .net project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GeolocationAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
